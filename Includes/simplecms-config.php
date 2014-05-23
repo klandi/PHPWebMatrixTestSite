842bd0f72d7d9befd97bfd8efee2cda2a046372e
klandi <oceanstone@gmail.com>
2014-05-23 14:56:05 -0400

    Kevin Initial Commit

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'KevMatrixDb');
+    define('DB_USER', 'klandi');
+    define('DB_PASSWORD', 'cecilia1');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'cecilia1');
+?>
